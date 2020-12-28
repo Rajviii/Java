7070d01fa16ed833666af720abb2f3a44a6313c1
Rajvi <76648851+Rajviii@users.noreply.github.com>
2020-12-28 16:11:45 +0530

    Addition and Multiplication in Java

---
+++ arithmetic_operators.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class arithmetic_operators {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 6;
+        System.out.print("The sum is: ");
+        int sum = (num1 + num2 + num3);
+        System.out.println(sum);
+
+        System.out.print("The multiplication is: ");
+        int mul= (num1 * num2 * num3);
+        System.out.println(mul);
+    }
+}
