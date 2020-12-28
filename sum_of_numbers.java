0f2a9e08800dcbd9b11a6a662537516ff488e8f5
Rajvi <76648851+Rajviii@users.noreply.github.com>
2020-12-28 16:10:37 +0530

    Let' do a simple maths in Java

---
+++ sum_of_numbers.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class sum_of_numbers {
+    public static void main(String[] args) {
+        System.out.print("The sum is:");
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 6;
+        int sum = (num1 + num2 + num3);
+        System.out.println(sum);
+    }
+}
