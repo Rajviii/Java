c23a081cb94450817c85e508767ee67c5299d3b2
Rajvi <76648851+Rajviii@users.noreply.github.com>
2020-12-28 16:19:21 +0530

    Demo of string 02

---
+++ demo_of_string_02.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+public class demo_of_string_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your String : ");
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
