97774deb4f63d2feea2af73bb40aebd852c02aea
Rajvi <76648851+Rajviii@users.noreply.github.com>
2020-12-28 16:18:23 +0530

    Demo of string 01

---
+++ demo_of_string_01.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+public class demo_of_string_01 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your String : ");
+      String name = sc.next();
+      System.out.println(name);
+    }
+}
