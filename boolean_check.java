5e5b8d6d2b88400b1b56331588b445e5f7d19f56
Rajvi <76648851+Rajviii@users.noreply.github.com>
2020-12-28 16:20:24 +0530

    Entered value is Int or not

---
+++ boolean_check.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+public class boolean_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value : ");
+        boolean a = sc.hasNextInt();
+        System.out.println(a);
+    }
+}
