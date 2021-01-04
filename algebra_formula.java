a97ef7e1c1baf77e25df04bb7cf59d6add48d630
Rajvi <76648851+Rajviii@users.noreply.github.com>
2021-01-04 16:14:34 +0530

    Formula

---
+++ algebra_formula.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class algebra_formula {
+    public static void main(String[] args) {
+        float v = 5;
+        float u = 4;
+        float a = 2;
+        float s = 1;
+        float x = (v*v - u*u) / (2*a*s);
+        System.out.println(x);
+    }
+}
