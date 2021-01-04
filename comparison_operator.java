ae62cdbe41d2f8787b9bdab43b3c3172ef80afa3
Rajvi <76648851+Rajviii@users.noreply.github.com>
2021-01-04 16:19:45 +0530

    Number Comparison

---
+++ comparison_operator.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+
+public class comparison_operator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Number : ");
+        int a = sc.nextInt();
+        System.out.println(a>10);
+    }
+}
