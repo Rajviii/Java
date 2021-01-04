c628d7d86675d59125011dce221867d9dcb75509
Rajvi <76648851+Rajviii@users.noreply.github.com>
2021-01-04 16:18:27 +0530

    Mathematical Associativity

---
+++ associativity_02.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class associativity_02 {
+    public static void main(String[] args) {
+        System.out.print("Answer a : ");
+        int a = 7*49/7+35/7; //Output is anyway correct because operation between int & int give the answer in int
+        System.out.println(a);
+    }
+}
